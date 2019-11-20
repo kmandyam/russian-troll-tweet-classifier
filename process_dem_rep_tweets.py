dc7987f6ab5cfc09fb768ed29944fed4d3cd980e
KARISHMA RAMESH MANDYAM <krm28@cs.washington.edu>
2019-11-20 11:52:56 -0800

    adds code to process dem_rep_tweets

---
+++ process_dem_rep_tweets.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+filename = "data/raw/democratvsrepublicantweets/ExtractedTweets.csv"
+df = pd.read_csv(filename)
+del df['Party']
+del df['Handle']
+
+def assign_label(row):
+    return 0
+
+
+df['label'] = df.apply(lambda row: assign_label(row), axis=1)
+df.to_csv("data/cleaned/dem_rep_tweets.csv", index=False)
+
+
+
